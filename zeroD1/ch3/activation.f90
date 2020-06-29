a75ec9f0943733a2149cfb24906195ac8ad89f6c
jj1guj <57407826+jj1guj@users.noreply.github.com>
2020-06-29 16:36:38 +0900

    ソフトマックス関数を追加

--- zeroD1/ch3/activation.f90
+++ zeroD1/ch3/activation.f90
@@ -81,4 +81,38 @@ contains
         !$omp end do
         !$omp end parallel
     end subroutine relu
+
+    subroutine softmax(x,y)
+        implicit none
+        real,intent(in) :: x(:,:)
+        real,allocatable,intent(out) ::  y(:,:)
+        real::exp_sum
+        real,allocatable::c(:,:),x2(:,:)
+        integer::i,j
+        integer,allocatable::Xshape(:)
+        Xshape=shape(x)
+        allocate(y(Xshape(1),Xshape(2)))
+        allocate(c(Xshape(1),Xshape(2)))
+        allocate(x2(Xshape(1),Xshape(2)))
+        
+        !オーバーフロー対策の下処理
+        c=maxval(x)
+        x2(:,:)=x(:,:)-c
+
+        exp_sum=sum(exp(x2))
+
+        !$omp parallel private(i)
+        !$omp do
+        do j=1,Xshape(1)
+            !$omp parallel
+            !$omp do
+            do i=1,Xshape(2)
+                y(j,i)=exp(x2(j,i))/exp_sum
+            end do
+            !$omp end do
+            !$omp end parallel
+        end do
+        !$omp end do
+        !$omp end parallel
+    end subroutine softmax
 end module activation
--- zeroD1/ch3/activation_main.f90
+++ zeroD1/ch3/activation_main.f90
@@ -1,16 +1,23 @@
 program main
     use activation
     implicit none
-    real::A(2,1)
+    real::A(2,1),A2(3,1)
     real,allocatable::B(:,:)
     A(1,1)=2
     A(2,1)=-1
+    A2=reshape((/1010,1000,990/),(/3,1/))
     call step_function(A,B)
     print*,B
+    deallocate(B)
 
     call sigmoid(A,B)
     print*,B
+    deallocate(B)
 
     call relu(A,B)
+    print*,B
+    deallocate(B)
+
+    call softmax(A2,B)
     print*,B
 end program main
