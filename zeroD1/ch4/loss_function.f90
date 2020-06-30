360fde1fe67c42c03520fb8e23f1306716f8da5a
jj1guj <57407826+jj1guj@users.noreply.github.com>
2020-06-30 12:39:12 +0900

    サブルーチンから関数に変えた

--- zeroD1/ch4/loss_function.f90
+++ zeroD1/ch4/loss_function.f90
@@ -2,40 +2,40 @@ module loss_function
     implicit none
     
 contains
-    subroutine meam_squared_error(Y,T,error)
+    function meam_squared_error(Y,T)
         implicit none
-        real,intent(in) :: Y(:,:)
-        integer,intent(in)::T(:,:)
-        real,intent(out) :: error
-        real::error_calc
-        integer::i,j,N
+        integer::T(:,:)
+        real::Y(:,:)
+        real::meam_squared_error,error_calc
+        integer i,j,N
         integer,allocatable::Yshape(:)
+
         Yshape=shape(Y)
         N=Yshape(2)
-        error=0
+        meam_squared_error=0
 
         do i=1,N
             error_calc=0
             do j=1,Yshape(1)
                 error_calc=error_calc+(Y(j,i)-real(T(j,i)))**2
             end do
-            error_calc=error_calc*0.5
-            error=error+error_calc
+            error_calc=0.5*error_calc
+            meam_squared_error=meam_squared_error+error_calc
         end do
-        error=error/real(N)
-    end subroutine meam_squared_error
+        meam_squared_error=meam_squared_error/real(N)
+    end function
 
-    subroutine cross_entropy_error(Y,T,error)
+    function cross_entropy_error(Y,T)
         implicit none
-        real,intent(in) :: Y(:,:)
-        integer,intent(in)::T(:,:)
-        real,intent(out) :: error
-        real::error_calc,delta
-        integer::i,j,N
+        integer::T(:,:)
+        real::Y(:,:)
+        real::cross_entropy_error,error_calc,delta
+        integer i,j,N
         integer,allocatable::Yshape(:)
+
         Yshape=shape(Y)
         N=Yshape(2)
-        error=0
+        cross_entropy_error=0
         delta=1e-7
 
         do i=1,N
@@ -43,8 +43,8 @@ contains
             do j=1,Yshape(1)
                 error_calc=error_calc-real(T(j,i))*log(Y(j,i)+delta)
             end do
-            error=error+error_calc
+            cross_entropy_error=cross_entropy_error+error_calc
         end do
-        error=error/real(N)
-    end subroutine cross_entropy_error
+        cross_entropy_error=cross_entropy_error/real(N)
+    end function
 end module loss_function
--- zeroD1/ch4/loss_function_main.f90
+++ zeroD1/ch4/loss_function_main.f90
@@ -1,14 +1,12 @@
 program main
     use loss_function
     implicit none
-    real::Y(10,1),ans
+    real::Y(10,1)
     integer::T(10,1)
     T=reshape((/0,0,1,0,0,0,0,0,0,0/),(/10,1/))
     Y=reshape((/0.1,0.05,0.6,0.0,0.05,0.1,0.0,0.1,0.0,0.0/),(/10,1/))
 
-    call meam_squared_error(Y,T,ans)
-    print*,ans
+    print*,meam_squared_error(Y,T)
 
-    call cross_entropy_error(Y,T,ans)
-    print*,ans
+    print*,cross_entropy_error(Y,T)
 end program main
