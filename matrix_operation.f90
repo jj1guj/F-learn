8398edd350f0e481f2faba1e2afd9da0091a0cd2
jj1guj <57407826+jj1guj@users.noreply.github.com>
2020-06-30 12:01:08 +0900

    サブルーチンから関数に変えた

--- matrix_operation.f90
+++ matrix_operation.f90
@@ -3,36 +3,30 @@ module matrix_operation
     
 contains
     !行列積を計算する
-    subroutine dot(A,B,C)
-        implicit none
-        real,intent(in)::A(:,:),B(:,:)
-        real,allocatable,intent(out)::C(:,:)
-        integer::i,j,k
+    function dot(A,B)
+        real::A(:,:),B(:,:)
+        real,allocatable::dot(:,:)
         integer::Ashape(2),Bshape(2)
+        integer::i,j,k
         Ashape=shape(A)
         Bshape=shape(B)
-        allocate(C(Bshape(1),Ashape(2)))
+        allocate(dot(Bshape(1),Ashape(2)))
 
-        if(Ashape(1)/=Bshape(2))then
-            print'(A)',"CAN NOT CALC!"
-        else
-            !$omp parallel private(j,k)
+        !$omp parallel private(j,k)
+        !$omp do
+        do i=1,Ashape(2)
+            !$omp parallel private(k)
             !$omp do
-            do i=1,Ashape(2)
-                !$omp parallel private(k)
-                !$omp do
-                do j=1,Bshape(1)
-                    C(j,i)=0
-                    do k=1,Ashape(1)
-                        C(j,i)=C(j,i)+A(k,i)*B(j,k)
-                    end do
+            do j=1,Bshape(1)
+                dot(j,i)=0
+                do k=1,Ashape(1)
+                    dot(j,i)=dot(j,i)+A(k,i)*B(j,k)
                 end do
-                !$omp end do
-                !$omp end parallel
             end do
             !$omp end do
             !$omp end parallel
-            return
-        end if
-    end subroutine dot
+        end do
+        !$omp end do
+        !$omp end parallel
+    end function
 end module matrix_operation
--- matrix_operation_main.f90
+++ matrix_operation_main.f90
@@ -2,11 +2,10 @@ program main
     use matrix_operation
     implicit none
     integer::ar,ac,br,bc
-    real,allocatable::A(:,:),B(:,:),C(:,:)
+    real,allocatable::A(:,:),B(:,:)
     read*,ar,ac,br,bc
     allocate(A(ac,ar))
     allocate(B(bc,br))
     read*,A,B
-    call dot(A,B,C)
-    print*,C
+    print*,dot(A,B)
 end program main
